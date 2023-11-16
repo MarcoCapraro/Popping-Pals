f690a538b082edfc8c93d4d565a3c0e3cec43ce1
MarcoCapraro <marco_capraro@yahoo.com>
2023-11-16 15:42:16 -0500

    Updated Main Menu Map Camera; Setup Win Condition and Simple Win UI
    Main Menu Camera is now at an appropriate distance to view the map. Win condition set up to count total amount of enemies needed to be destroyed. When enemies destroyed, counter decrements until it hits 0. Once at 0 the player is prompted with a You Won! UI screen.

---
+++ PoppingPals 5.3/Intermediate/Build/Mac/UnrealEditor/Inc/PoppingPals/UHT/EnemyT1.gen.cpp
@@ -0,0 +1,91 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "PoppingPals/EnemyT1.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeEnemyT1() {}
+// Cross Module References
+	POPPINGPALS_API UClass* Z_Construct_UClass_ABaseEnemy();
+	POPPINGPALS_API UClass* Z_Construct_UClass_AEnemyT1();
+	POPPINGPALS_API UClass* Z_Construct_UClass_AEnemyT1_NoRegister();
+	UPackage* Z_Construct_UPackage__Script_PoppingPals();
+// End Cross Module References
+	void AEnemyT1::StaticRegisterNativesAEnemyT1()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(AEnemyT1);
+	UClass* Z_Construct_UClass_AEnemyT1_NoRegister()
+	{
+		return AEnemyT1::StaticClass();
+	}
+	struct Z_Construct_UClass_AEnemyT1_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AEnemyT1_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_ABaseEnemy,
+		(UObject* (*)())Z_Construct_UPackage__Script_PoppingPals,
+	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AEnemyT1_Statics::DependentSingletons) < 16);
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AEnemyT1_Statics::Class_MetaDataParams[] = {
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "/**\n * \n */" },
+#endif
+		{ "IncludePath", "EnemyT1.h" },
+		{ "ModuleRelativePath", "EnemyT1.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AEnemyT1_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AEnemyT1>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_AEnemyT1_Statics::ClassParams = {
+		&AEnemyT1::StaticClass,
+		"Engine",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AEnemyT1_Statics::Class_MetaDataParams), Z_Construct_UClass_AEnemyT1_Statics::Class_MetaDataParams)
+	};
+	UClass* Z_Construct_UClass_AEnemyT1()
+	{
+		if (!Z_Registration_Info_UClass_AEnemyT1.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_AEnemyT1.OuterSingleton, Z_Construct_UClass_AEnemyT1_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_AEnemyT1.OuterSingleton;
+	}
+	template<> POPPINGPALS_API UClass* StaticClass<AEnemyT1>()
+	{
+		return AEnemyT1::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AEnemyT1);
+	AEnemyT1::~AEnemyT1() {}
+	struct Z_CompiledInDeferFile_FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT1_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT1_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_AEnemyT1, AEnemyT1::StaticClass, TEXT("AEnemyT1"), &Z_Registration_Info_UClass_AEnemyT1, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AEnemyT1), 481102245U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT1_h_2336635609(TEXT("/Script/PoppingPals"),
+		Z_CompiledInDeferFile_FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT1_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT1_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ PoppingPals 5.3/Intermediate/Build/Mac/UnrealEditor/Inc/PoppingPals/UHT/EnemyT4.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "EnemyT4.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef POPPINGPALS_EnemyT4_generated_h
+#error "EnemyT4.generated.h already included, missing '#pragma once' in EnemyT4.h"
+#endif
+#define POPPINGPALS_EnemyT4_generated_h
+
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_SPARSE_DATA
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_ACCESSORS
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEnemyT4(); \
+	friend struct Z_Construct_UClass_AEnemyT4_Statics; \
+public: \
+	DECLARE_CLASS(AEnemyT4, ABaseEnemy, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/PoppingPals"), NO_API) \
+	DECLARE_SERIALIZER(AEnemyT4)
+
+
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemyT4(AEnemyT4&&); \
+	NO_API AEnemyT4(const AEnemyT4&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemyT4); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemyT4); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEnemyT4) \
+	NO_API virtual ~AEnemyT4();
+
+
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_12_PROLOG
+#define FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_SPARSE_DATA \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_ACCESSORS \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_INCLASS_NO_PURE_DECLS \
+	FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> POPPINGPALS_API UClass* StaticClass<class AEnemyT4>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_marco_capraro_Documents_GitHub_Popping_Pals_PoppingPals_5_3_Source_PoppingPals_EnemyT4_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
