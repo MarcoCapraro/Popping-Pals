b0cae563827a0f37f85428d10885f8021cdb053f
Gianluca Capraro <gmcapra@umich.edu>
2023-12-21 17:19:28 -0500

    Add New Fonts, Update Main Menu Font

--- PoppingPals 5.3/Intermediate/Build/Mac/x64/PoppingPalsEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "/Users/marco_capraro/Documents/GitHub/Popping-Pals/PoppingPals 5.3/Intermediate/Build/Mac/x64/PoppingPalsEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
