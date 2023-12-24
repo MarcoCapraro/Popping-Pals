8f9df48b90effa71feef1f0d93f9d1d075d9f3ea
MarcoCapraro <marco_capraro@yahoo.com>
2023-12-24 11:23:54 -0500

    Updated gitIgnore Cache

--- PoppingPals 5.3/Intermediate/Build/Mac/arm64/PoppingPalsEditor/Development/Slate/SharedPCH.Slate.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "/Users/gianlucacapraro/Documents/GitHub/Popping-Pals/PoppingPals 5.3/Intermediate/Build/Mac/arm64/PoppingPalsEditor/Development/Slate/SharedDefinitions.Slate.Cpp17.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
